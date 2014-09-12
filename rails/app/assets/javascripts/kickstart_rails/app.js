ee8423062fe31ebda84fe57d84e1b66886175690
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-09-12 09:52:31 -0400

    removing useless files
    Former-commit-id: c6d632628aebe210ea05f2f00699e6261f86d08f

--- rails/app/assets/javascripts/kickstart_rails/app.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var KS, Modal;
-
-  KS = require('./ks');
-
-  Modal = require('./modal');
-
-}).call(this);
--- rails/app/assets/javascripts/kickstart_rails/ks.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  global.k$ = new Object();
-
-  module.exports = k$;
-
-}).call(this);
