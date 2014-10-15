f9fc9688855700f7c83b44a2f2c66bc319d91a90
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-10-14 21:17:29 -0400

    Docs fixing works now

--- lib/coffee/navbar-fixer.coffee
+++ lib/coffee/navbar-fixer.coffee
@@ -1,1 +1,9 @@
-console.log 'ping from navbar fix'
+window.onscroll = ->
+  $marker = k$.$('#marker')
+  $fixedArea = k$.$('.fixed-area')
+  $offset = $marker.getBoundingClientRect().top
+  $isFixed = $fixedArea.classList.contains 'fixed'
+  if $offset < 1
+    $fixedArea.classList.add 'fixed' if not $isFixed
+  else
+    $fixedArea.classList.remove 'fixed' if $isFixed
--- rails/app/assets/javascripts/kickstart_rails/navbar-fixer.js
+++ rails/app/assets/javascripts/kickstart_rails/navbar-fixer.js
@@ -1,4 +1,16 @@
 (function() {
-  console.log('ping from navbar fix');
+  window.onscroll = function() {
+    var $fixedArea, $isFixed, $marker, $offset;
+    $marker = k$.$('#marker');
+    $fixedArea = k$.$('.fixed-area');
+    $offset = $marker.getBoundingClientRect().top;
+    $isFixed = $fixedArea.classList.contains('fixed');
+    if ($offset < 1) {
+      if (!$isFixed) {
+        $fixedArea.classList.add('fixed');
+      }
+      return $marker.scrollTop = '-50px';
+    }
+  };
 
 }).call(this);
