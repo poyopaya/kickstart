008bd20a6172d42ed45bafadb63c5080daac1981
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-10-27 16:40:35 -0400

    Fixing ref to test.js

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -7,8 +7,8 @@ var coffee         = require('gulp-coffee');
 var expect         = require('gulp-expect-file');
 
 
-gulp.task('docs:test', ['docs:bundle-tests', 'docs:jade'], function() {
-  var files = ['docs/tests/index.html']
+gulp.task('test', ['bundle-tests', 'jade'], function() {
+  var files = ['public/tests/index.html']
   gulp.src(files)
     .pipe(mochaPhantomJS({reporter: 'list'}))
 });
