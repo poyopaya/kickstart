7d0fcfab06b5b97c761cbec18f87ddd5a9299db8
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-10-27 21:41:13 -0400

    Shrinking kickstart.css

--- gulp/tasks/docs/bundle-kickstart.js
+++ gulp/tasks/docs/bundle-kickstart.js
@@ -1,8 +1,8 @@
 var browserify     = require('browserify');
 var watchify       = require('watchify');
-var bundleLogger   = require('../util/bundleLogger');
+var bundleLogger   = require('../../util/bundleLogger');
 var gulp           = require('gulp');
-var handleErrors   = require('../util/handleErrors');
+var handleErrors   = require('../../util/handleErrors');
 var source         = require('vinyl-source-stream');
 
 // Bundles kickstart.js
