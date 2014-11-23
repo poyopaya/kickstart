014352820d0e623f2cd10cedc94fd98a5e488523
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-11-23 18:27:16 -0500

    Adding 'watch:all' option

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,3 +6,10 @@ gulp.task('watch', ['setWatch', 'browserSync'], function() {
     gulp.watch('./lib/jade/**/*.jade', ['jade']);
     gulp.watch(['./lib/jade/tests/**', './lib/tests/**'], ['test']);
 });
+
+gulp.task('watch:all', ['setWatch', 'docs:setWatch', 'browserSync', 'docs:browserSync'], function() {
+    gulp.watch(['./{lib, lib-docs, lib-core}/sass/**/*.sass'], ['sass', 'minify', 'docs:sass', 'docs:minify']);
+    gulp.watch(['./{lib, lib-docs, lib-core}/images/**'], ['images', 'docs:images']);
+    gulp.watch(['./{lib, lib-docs, lib-core}/jade/**/*.jade'], ['jade', 'docs:jade']);
+    gulp.watch(['./{lib, lib-docs, lib-core}/jade/tests/**', './lib/tests/**'], ['test', 'docs:test']);
+});
