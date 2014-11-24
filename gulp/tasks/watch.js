61e0a5b4ddc15af48f8c1b8acd26aafce44f60d5
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-11-24 10:11:56 -0500

    Fixing watch in the right place

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,6 +4,7 @@ gulp.task('watch', ['setWatch', 'browserSync'], function() {
     gulp.watch('./lib/sass/**/*.sass', ['sass', 'minify']);
     gulp.watch('./lib/images/**', ['images']);
     gulp.watch('./lib/jade/**/*.jade', ['jade']);
+    gulp.watch(['./lib/coffee/**/*.coffee'], ['browserify', 'minify']);
     gulp.watch(['./lib/jade/tests/**', './lib/tests/**'], ['test']);
 });
 
@@ -11,6 +12,5 @@ gulp.task('watch:all', ['setWatch', 'docs:setWatch', 'browserSync', 'docs:browse
     gulp.watch(['./{lib, lib-docs, lib-core}/sass/**/*.sass'], ['sass', 'minify', 'docs:sass', 'docs:minify']);
     gulp.watch(['./{lib, lib-docs, lib-core}/images/**'], ['images', 'docs:images']);
     gulp.watch(['./{lib, lib-docs, lib-core}/jade/**/*.jade'], ['jade', 'docs:jade']);
-    gulp.watch(['./lib/coffee/**/*.coffee'], ['browserify', 'minify']);
     gulp.watch(['./{lib, lib-docs, lib-core}/jade/tests/**', './lib/tests/**'], ['test', 'docs:test']);
 });
