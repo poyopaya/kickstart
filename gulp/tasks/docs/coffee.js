c6f1033edde14791c661d9c0053a08d41b84b2ca
Adam Kochanowicz <ajkochanowicz@gmail.com>
2015-05-28 16:08:40 -0400

    Adding some copy to default js file

--- gulp/tasks/docs/coffee.js
+++ gulp/tasks/docs/coffee.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp'),
   gulpif = require('gulp-if'),
   uglify = require('gulp-uglify'),
+  babel  = require('gulp-babel'),
   coffee = require('gulp-coffee');
 
 gulp.task('docs:coffee', function() {
   gulp.src(['./lib-docs/coffee/navbar-fixer.coffee', './lib-docs/coffee/index.coffee', './lib-docs/js/**/*.js'])
     .pipe(gulpif(/[.]coffee$/, coffee()))
     .pipe(gulp.dest('./docs/js'));
-    
+
   gulp.src(['./lib-docs/js/**/*.js'])
     .pipe(babel())
     .pipe(gulp.dest('./docs/js'));
